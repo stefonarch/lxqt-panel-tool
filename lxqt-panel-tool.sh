3f3a65107c1ce9e0bc976a0b5cd1e77b1da39947
stefonarch <standreas@riseup.net>
2026-01-02 20:59:59 +0100

    Use own directory and import

--- lxqt-panel-tool.sh
+++ lxqt-panel-tool.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-if [ ! -d "$XDG_DATA_HOME/lxqt-panel-profiles/" ]; then
-    mkdir -p $XDG_DATA_HOME/lxqt-panel-profiles/
-    #cp -r /usr/share/lxqt-panel-tools/layouts $XDG_DATA_HOME/lxqt-panel-profiles
-    mkdir -p $XDG_DATA_HOME/lxqt-panel-profiles/layouts/lxqt-panel/
-    mkdir -p $XDG_DATA_HOME/lxqt-panel-profiles/layouts/Default\ panel/
-    cp /etc/xdg/lxqt/panel.conf $XDG_DATA_HOME/lxqt-panel-profiles/layouts/Default\ panel/
-    cp $XDG_CONFIG_HOME/lxqt/panel.conf $XDG_DATA_HOME/lxqt-panel-profiles/layouts/lxqt-panel/
-    cp $XDG_CONFIG_HOME/lxqt/panel.conf $XDG_DATA_HOME/lxqt-panel-profiles/layouts/xqt-panel/
-    cp -a $XDG_DATA_HOME/lxqt-panel-profiles/layouts/lxqt-panel/ $XDG_DATA_HOME/lxqt-panel-profiles/layouts/In\ use:\ lxqt-panel/
+if [ ! -d "$XDG_DATA_HOME/lxqt-panel-tool/" ]; then
+    mkdir -p $XDG_DATA_HOME/lxqt-panel-tool/
+    #cp -r /usr/share/lxqt-panel-tools/layouts $XDG_DATA_HOME/lxqt-panel-tool
+    mkdir -p $XDG_DATA_HOME/lxqt-panel-tool/layouts/panel-"$USER"/
+    mkdir -p $XDG_DATA_HOME/lxqt-panel-tool/layouts/Default\ panel/
+    cp /etc/xdg/lxqt/panel.conf $XDG_DATA_HOME/lxqt-panel-tool/layouts/Default\ panel/
+    cp $XDG_CONFIG_HOME/lxqt/panel.conf $XDG_DATA_HOME/lxqt-panel-tool/layouts/panel-"$USER"/
+    cp -a $XDG_DATA_HOME/lxqt-panel-tool/layouts/panel-"$USER"/ $XDG_DATA_HOME/lxqt-panel-tool/layouts/In\ use:\ panel-"$USER"/
 
+    # Import lxqt-panel-profiles
+    if [ -d "$XDG_DATA_HOME/lxqt-panel-profiles/" ]; then
+        echo "Found lxqt-panel-profiles, importing..."
+    cp -av $XDG_DATA_HOME/lxqt-panel-profiles/layouts/* $XDG_DATA_HOME/lxqt-panel-tool/layouts/
+    fi
 fi
 
 # first try local installation
 
-if [ -f "$HOME/.local/share/lxqt-panel-tool/lxqt-panel-tool.py" ]; then
-    exec python3 "$HOME/.local/share/lxqt-panel-tool/lxqt-panel-tool.py"
+if [ -f "$XDG_DATA_HOME/lxqt-panel-tool/lxqt-panel-tool.py" ]; then
+    exec python3 "$XDG_DATA_HOME/lxqt-panel-tool/lxqt-panel-tool.py"
 else
     exec python3 "/usr/share/lxqt-panel-tool/lxqt-panel-tool.py"
 fi
